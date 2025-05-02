ae1e6e47f70f2ae292f2893132db13d5b28ca356
PLBS Lahari <2024eb01755@online.bits-pilani.ac.in>
2025-05-02 16:20:19 +0530

    Final commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css';
+// import { createContext, useContext, useState } from 'react';
+
+// const BuildContext = createContext();
+
+
+function App() {
+  // const [bulbOn, setBulbOn] = useState(true);
+    return (
+      // <BuildContext.Provider value={{bulbOn, setBulbOn}}>
+      //   <Light />
+      // </BuildContext.Provider>
+      <h1>Hi</h1>
+    )
+
+}
+
+// function Light(){
+//   return(
+//     <div>
+//       <LightOn />
+//       <LightSwitch />
+//     </div>
+//   )
+// }
+
+// function LightOn(){
+//   const {bulbOn} = useContext(BuildContext);
+  
+//   return(
+//     <div className="light-on">
+//       {bulbOn ? "Bulb is ON" : "Bulb is OFF"}
+//     </div>
+//   )
+// }
+
+// function LightSwitch(){
+//   const {bulbOn, setBulbOn} = useContext(BuildContext);
+//   return(
+//     <div className="light-switch">
+//       <button onClick={() => setBulbOn(!bulbOn)}>
+//         {/* {bulbOn ? "OFF" : "ON"} */}
+//         Toggle
+//       </button>
+//     </div>
+//   )
+// }
+
+export default App;
